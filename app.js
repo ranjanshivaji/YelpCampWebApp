ab030fdf432d5ac8fe245e3fa3009647492190c9
ranjanshivaji <ranjanshivaji172@gmail.com>
2020-05-25 03:21:53 +0530

    remove responsive error

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 var express = require('express'),
 	app = express(),
 	bodyParser = require('body-parser'),
@@ -18,15 +18,9 @@ var commentRoutes = require('./routes/comments'),
 	indexRoutes = require('./routes/index');
 
 //seedDB(); //Seed The data Base
-
-//mongoose.set('useUnifiedTopology', true);
-
-// mongoose.connect('mongodb+srv://ranjan:ranjan123@ranjan-lst5i.mongodb.net/test?retryWrites=true&w=majority', {
-// 	useNewUrlParser: true,
-// 	useUnifiedTopology: true
-// });
-//console.log(process.env.DATABASEURL);
-mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true, useUnifiedTopology: true });
+var url = process.env.DATABASEURL || 'mongodb://localhost:27017/yelp_camp_V10';
+//mongodb+srv://ranjan:ranjan123@ranjan-lst5i.mongodb.net/test?retryWrites=true&w=majority
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
@@ -65,6 +59,6 @@ app.use('/campgrounds/:id/comments', commentRoutes);
 //                                                                               //
 // ============================================================================= //
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(port, function() {
 	console.log('Yelp Camp V12 Has started');
 });
